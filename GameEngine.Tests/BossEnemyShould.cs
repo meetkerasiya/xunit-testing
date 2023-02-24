ad633dd7c02d6d8f1de8fe37a8eaa90581d33652
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-24 12:03:23 +0530

    assert for floating point added

---
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -0,0 +1,13 @@
+﻿namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorretPower()
+        {
+            BossEnemy sut= new BossEnemy();
+
+            Assert.Equal(166.667,sut.TotalSpecialAttackPower, 3); //precision of 3, but dong that might do round figuring
+        }
+    }
+}
